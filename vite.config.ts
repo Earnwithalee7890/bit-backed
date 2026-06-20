57cf2b0a311eaeda07bd7831b60dfea6ccfe4665
earnwithalee <earnwithalee@gmail.com>
2026-06-21 01:52:55 +0500

    chore: optimize production build assets and resolve package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@stacks')) {
+              return 'vendor-stacks';
+            }
+            if (id.includes('@walletconnect') || id.includes('@web3modal') || id.includes('reown') || id.includes('ox_esm')) {
+              return 'vendor-walletconnect';
+            }
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor-react';
+            }
+            return 'vendor-others';
+          }
+        }
+      }
+    }
+  }
 })
